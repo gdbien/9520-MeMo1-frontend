4980fb897dba201d4dc7b6afdc131643446239bb
“Nicolas <nicolas.bugliot@mercadolibre.com>
2021-02-20 18:41:44 -0300

    add router and start-dev script

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <h1> Hello world </h1>
+    <Router>
+      <Switch>
+        <Route path="/support">
+          <h1> Soporte </h1>
+        </Route>
+        <Route path="/resources">
+          <h1> Recrusos </h1>
+        </Route>
+        <Route path="/hours">
+          <h1> Carga de horas </h1>
+        </Route>
+        <Route path="/">
+          <h1> Home </h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
