bb5a6b23870c23546e4c0757890d85e193912ad9
benjaortiz <benjaortiz969@gmail.com>
2021-02-25 16:47:15 -0300

    update NavBar
    ya no es transparente

--- src/CargaDeHoras/Componentes/NavBar.jsx
+++ src/CargaDeHoras/Componentes/NavBar.jsx
@@ -2,25 +2,36 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import {makeStyles} from '@material-ui/core/styles';
+import { Link } from 'react-router-dom'
 
-import {
-    Link
-} from 'react-router-dom'
+const useStyles = makeStyles({
+    barra: {
+        backgroundColor: "#88eaf1"
+    },
+    flechitas: {
+        color: "#000000",
+        fontSize: "small"
+    },
+  });
+
 
 const Navbar = (props) => {
 
+var classes = useStyles();
+
 
     return (
-        <AppBar color='transparent'>
+        <AppBar className={classes.barra} >
             <Toolbar>
 
-                <ArrowForwardIosIcon fontSize="small"/>
+                <ArrowForwardIosIcon className={classes.flechitas}/>
 
                 <Link to="/">
                      Inicio
                 </Link>
 
-                <ArrowForwardIosIcon fontSize="small"/>
+                <ArrowForwardIosIcon className={classes.flechitas}/>
 
                 <Link to="/resources">
                      Recursos
