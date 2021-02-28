e3b603f63c5f8e70354940b7c78e44dc046a2db0
Francisco <franciscojnasif@gmail.com>
2021-02-28 05:08:04 -0300

    Alta de tareas terminada

--- src/Proyectos/TasksList.js
+++ src/Proyectos/TasksList.js
@@ -6,15 +6,17 @@ import {
     FormControl,
     IconButton,
     Input,
-    InputLabel, MenuItem, Select,
+    InputLabel, Select,
     Typography
 } from "@material-ui/core";
-import Modal from "@material-ui/core/Modal";
 import {makeStyles} from "@material-ui/core/styles";
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import {green} from "@material-ui/core/colors";
 import Button from "@material-ui/core/Button";
-import SendIcon from '@material-ui/icons/Send';
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import Dialog from "@material-ui/core/Dialog";
 
 
 const columns = [
@@ -27,20 +29,8 @@ const columns = [
     { field: 'totalHours', headerName: 'Horas Totales', width: 170 },
     { field: 'creationDate', headerName: 'Fecha de Creacion', width: 190 },
     { field: 'projectId', headerName: 'Project ID', width: 120 },
-    { field: 'resources', headerName: 'Recursos', width: 150 },
-    { field: 'tickets', headerName: 'Tickets', width: 150 }
+    { field: 'person', headerName: 'Encargado', width: 150 },
 ];
-
-function getModalStyle() {
-    const top = 50;
-    const left = 50;
-
-    return {
-        top: `${top}%`,
-        left: `${left}%`,
-        transform: `translate(-${top}%, -${left}%)`,
-    };
-}
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -67,24 +57,24 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+
 export default function TasksList(props) {
 
     const classes = useStyles();
-    const [modalStyle] = React.useState(getModalStyle);
     const [open, setOpen] = React.useState(false);
 
-    const [error, setError] = React.useState(null);
-    const [isLoaded, setIsLoaded] = React.useState(false);
     const [tasks, setTasks] = React.useState([]);
     const [name, setName] = React.useState(null);
 
+    const [persons, setPersons] = React.useState([]);
 
     var new_task = {
-        "projectId":0,
-        "name":"",
-        "description":"",
-        "estimation":0,
-        "totalHours":0
+        "projectId": 0,
+        "name": "",
+        "description": "",
+        "estimation": 0,
+        "totalHours": 0,
+        "resourceName":"",
     };
 
     new_task.projectId = props.projectId;
@@ -97,88 +87,116 @@ export default function TasksList(props) {
         setOpen(false);
     };
 
-
-
     const createNewTask = () => {
         const requestOptions = {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(new_task)
         };
         fetch('https://psa-projects.herokuapp.com/tasks', requestOptions)
-            .then(r => tasks.push(r));
+
         handleClose();
+        /*window.location.reload(false);*/
+    };
+
+    const handleChangeSelect = (event) => {
+        new_task.resourceName=event.target.value;
+    };
+
+    const handleChangeName = (event) => {
+        new_task.name=event.target.value;
+    };
+
+    const handleChangeDesc = (event) => {
+        new_task.description = event.target.value;
+    };
+
+    const handleChangeHours = (event) => {
+        new_task.totalHours = event.target.value;
+    };
+
+    const handleChangeEst = (event) => {
+        new_task.estimation = event.target.value;
     };
 
     const body = (
-        <div style={modalStyle} className={classes.paper}>
-            <h1 id="title" align="center"> Nueva Tarea </h1>
-            <form className={classes.create} noValidate autoComplete="off">
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="name">Nombre</InputLabel>
-                    <Input id="name" onChange={event => new_task.name = event.target.value} />
-                </FormControl>
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="description">Descripcion</InputLabel>
-                    <Input id="description" onChange={event => new_task.description = event.target.value} />
-                </FormControl>
-                <FormControl className={classes.formControl}>
-                    <InputLabel id="priority-label">Prioridad</InputLabel>
-                    <Select
-                        labelId="priority-label"
-                        id="priority"
-                        onChange={event => new_task.priority = event.target.value}
-                    >
-                        <MenuItem value="BAJA">Baja</MenuItem>
-                        <MenuItem value="MEDIA">Media</MenuItem>
-                        <MenuItem value="ALTA">Alta</MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="estimation">Estimación</InputLabel>
-                    <Input id="estimation" onChange={event => new_task.estimation = event.target.value}
-                           type="number"/>
-                </FormControl>
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="totalHours">Horas Totales</InputLabel>
-                    <Input id="totalHours" onChange={event => new_task.totalHours = event.target.value}
-                           type="number"/>
-                </FormControl>
-            </form>
-            <Button
-                variant="contained"
-                color="primary"
-                className={classes.margin}
-                endIcon={<SendIcon />}
-                onClick={createNewTask}
-            >
-                Aceptar
-            </Button>
+        <div>
+            <Dialog disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose}>
+                <DialogTitle>Nueva Tarea</DialogTitle>
+                <DialogContent>
+                    <form className={classes.container}>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="text-name"> Nombre </InputLabel>
+                            <Input id="text-name" aria-describedby="name" onChange={handleChangeName}/>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="text-desc"> Descripcion </InputLabel>
+                            <Input id="text-desc" aria-describedby="desc" onChange={handleChangeDesc}/>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="text-total-hours"> Horas Totales </InputLabel>
+                            <Input id="text-total-hours" aria-describedby="total-hours"
+                                   type="number" onChange={handleChangeHours}/>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="text-estimation"> Horas Totales </InputLabel>
+                            <Input id="text-estimation" aria-describedby="estimation"
+                                   type="number" onChange={handleChangeEst}/>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="select-encargado"> Encargado </InputLabel>
+                            <Select
+                                native
+                                value={new_task.resourceName}
+                                onChange={handleChangeSelect}
+                                input={<Input id="select-encargado"/>}
+                            >
+                                {persons.map((person) => (
+                                    <option value={
+                                        {
+                                            "legajo": person.numLegajo,
+                                            "name": person.nombre + ' ' + person.apellido
+                                        }
+                                    }>
+                                        {person.nombre + ' ' + person.apellido}
+                                    </option>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </form>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={createNewTask} color="primary">
+                        Ok
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 
     React.useEffect(() => {
-        fetch('https://psa-projects.herokuapp.com/projects/project?id='+props.projectId)
+        fetch('https://psa-projects.herokuapp.com/projects/project?id=' + props.projectId)
             .then(res => res.json())
             .then(
                 (result) => {
-                    setIsLoaded(true);
                     setTasks(result.tasksList);
                     setName(result.name);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
                 }
             )
     },[props.projectId])
 
-    if (error) {
-        return <div> Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div> Loading...</div>;
-    }
-
+    React.useEffect(() => {
+        fetch('https://psa-bac-carga-de-horas.herokuapp.com/personas')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setPersons(result);
+                }
+            )
+    },[])
 
     return (
         <React.Fragment>
@@ -192,23 +210,16 @@ export default function TasksList(props) {
                 <IconButton aria-label="new" className={classes.margin} onClick={handleOpen}>
                     <AddCircleIcon fontSize="large" style={{ color: green[500] }}/>
                 </IconButton>
-                <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="simple-modal-title"
-                    aria-describedby="simple-modal-description"
-                >
-                    {body}
-                </Modal>
+                {body}
             </Container>
             <Container fixed>
                 <div style={{ height: 400, width: '100%' }}>
                     <DataGrid rows={tasks.map((task) => {
                         task["id"] = task.taskId;
                         return task;
-                    })} columns={columns} pageSize={5} checkboxSelection />
+                    })} columns={columns} pageSize={5} />
                 </div>
             </Container>
         </React.Fragment>
-    )
+    );
 }
